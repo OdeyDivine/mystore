67dd757ab14782b221f507b196070d0d77b63469
Divine <odeydivine16@gmail.com>
2025-03-16 16:48:49 +0100

    hello

--- code.js
+++ code.js
@@ -1,6 +1,8 @@
 const menu = document.getElementById('menu'); 
 const navList = document.getElementById('visible'); 
 const closed =document.getElementById('closed')
+const foodLink = document.getElementById('chop')
+const foodSection = document.getElementById('eatable');
 
 menu.addEventListener('click', () => {
      navList.classList.toggle('show');
@@ -11,3 +13,9 @@ closed.addEventListener("click", () => {
 });
 
 
+
+foodLink.addEventListener("click", (e) => {
+    e.preventDefault(); 
+    foodSection.scrollIntoView({ behavior: 'smooth' }); 
+    navList.classList.remove("show"); 
+});
